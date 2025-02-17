9226f98a55ba1d85f37a5cda600b555a46660413
Ferlat Thibaud <thibaud.ferlat@student.isartdigital.com>
2025-02-17 16:43:14 +0100

    Level Loader Proto
    -Add Json with ascci levels
    -Add working level loader
    -Moved level loading from the grid manager to the level loader

--- Scripts/Json/LevelLoader.cs
+++ Scripts/Json/LevelLoader.cs
@@ -1,3 +1,6 @@
+using Com.IsartDigital.SokoVolt.GameObjects;
+using Com.IsartDigital.SokoVolt.GameObjects.Movables;
+using Com.IsartDigital.SokoVolt.Managers;
 using Godot;
 using System;
 
@@ -6,6 +9,12 @@ namespace Com.IsartDigital.SokoVolt {
 	
 	public partial class LevelLoader : Node
 	{
+		[Export] private PackedScene cellScene, playerScene, boxScene, wallScene, electricWallScene, goalBulbScene, generatorScene, doorScene; 
+		[Export] private Node2D objectsContainer;  
+
+
+		private const string LEVELS_JSONS_PATH = "res://Scripts/Json/Levels.json"; 
+
 		#region GetInstance
 		static private LevelLoader instance;
 		
@@ -27,13 +36,114 @@ namespace Com.IsartDigital.SokoVolt {
 		}
 		instance = this;
 		#endregion
-
+			LoadLevel(3); 
 		}
 
-		public override void _Process(double pDelta)
+		public void LoadLevel(int pLevel)
 		{
+			string lJsonContent = JsonTool.ReadFileContents(LEVELS_JSONS_PATH);
 
-		}
+			 // Parser le JSON
+			if (!JsonTool.TryParseJson(lJsonContent, out Godot.Collections.Dictionary lRootDict))
+			{
+				GD.PrintErr("Erreur : Impossible de parser le fichier JSON.");
+				return;
+			}
+
+			// Vérifier si le JSON contient les niveaux
+			if (!lRootDict.ContainsKey("levelDesign"))
+			{
+				GD.PrintErr("Erreur : Pas de clé 'levelDesign' dans le JSON.");
+				return;
+			}
+
+			Godot.Collections.Array lLevelList = (Godot.Collections.Array)lRootDict["levelDesign"]; 
+
+			if (pLevel < 0 || pLevel >= lLevelList.Count)
+			{
+				GD.PrintErr($"Erreur : Index de niveau invalide ({pLevel}).");
+				return;
+			}
+
+			// Récupérer le niveau sélectionné
+			Godot.Collections.Dictionary levelData = (Godot.Collections.Dictionary)lLevelList[pLevel];
+
+			// Récupérer la map
+			Godot.Collections.Array mapArray = (Godot.Collections.Array)levelData["map"];
+
+			// Convertir la map en tableau de strings
+			string[] levelMap = new string[mapArray.Count];
+			for (int i = 0; i < mapArray.Count; i++)
+			{
+				levelMap[i] = mapArray[i].ToString();
+			}
+
+
+			int levelHeight = mapArray.Count;
+			int levelWidth = mapArray.Count > 0 ? mapArray[0].ToString().Length : 0;
+
+			// Redimensionner la grille dynamiquement
+			GridManager.GetInstance().grid = new Cell[levelWidth, levelHeight];
+
+			GD.Print($"Chargement du niveau {pLevel} - Taille : {levelWidth}x{levelHeight}");
+
+			// Charger le niveau
+			for (int y = 0; y < levelHeight; y++)
+			{
+				string row = mapArray[y].ToString();
+
+				for (int x = 0; x < levelWidth; x++)
+				{
+					if (x >= row.Length) continue; // Évite les erreurs si une ligne est plus courte
+
+					char lTile = row[x];
+
+					Cell lCell = Utils.Spawner(cellScene, x, y, objectsContainer) as Cell;
+					GridManager.GetInstance().grid[x, y] = lCell;
+
+					GameObject lObj = null; 
+
+					switch(lTile)
+					{
+						case '@':
+							lObj = Utils.Spawner(playerScene, x, y, objectsContainer) as Player;
+							GridManager.GetInstance().player = lObj as Player;
+							break;
+						case '$':
+							lObj = Utils.Spawner(boxScene, x, y, objectsContainer) as BoxTesla;
+							break;
+						case '#':
+							lObj = Utils.Spawner(wallScene, x, y, objectsContainer) as Wall;
+							break;
+						case '*':
+							lObj = Utils.Spawner(electricWallScene, x, y, objectsContainer) as ElectricWall;
+							break;
+						case '.':
+							lObj = Utils.Spawner(goalBulbScene, x, y, objectsContainer) as GoalBulb;
+							break;
+						case '/':
+							lObj = Utils.Spawner(generatorScene, x, y, objectsContainer) as Generator;
+							break;
+						case '|':
+							lObj = Utils.Spawner(doorScene, x, y, objectsContainer) as Door;
+							break;
+					}
+
+					if (lObj != null)
+					{
+						lCell.SetContent(lObj);
+						lObj.SetCell(lCell);
+						lObj.Init(x, y);
+					}
+				}
+			}
+			}
+
+			
+
+
+
+		
 
 		#region dispose
 		protected override void Dispose(bool pDisposing)
--- Scripts/Managers/GridManager.cs
+++ Scripts/Managers/GridManager.cs
@@ -19,9 +19,7 @@ namespace Com.IsartDigital.SokoVolt.Managers {
 		private GridManager ():base() {}
 		#endregion
 		
-		[Export] private PackedScene cellScene, playerScene, boxScene, wallScene, electricWallScene, goalBulbScene, generatorScene, doorScene; 
 
-		[Export] private Node2D objectsContainer;  
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -30,9 +28,9 @@ namespace Com.IsartDigital.SokoVolt.Managers {
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-		public Cell[,] grid { get; private set; } = new Cell[GRID_WIDTH, GRID_HEIGHT];
+		public Cell[,] grid { get; set; } = new Cell[GRID_WIDTH, GRID_HEIGHT];
 		public static Vector2 gridOffset; 
-		private Player player;
+		public Player player;
 
 
 		public override void _Ready()
@@ -58,74 +56,74 @@ namespace Com.IsartDigital.SokoVolt.Managers {
         public override void Init()
         {
             base.Init();
-            LoadLevel();
+            // LoadLevel();
         }
 
-        public void LoadLevel() // ====================================> A basculer dans un LevelLoader
-		{
-			CenterGrid();
+        // public void LoadLevel() // ====================================> A basculer dans un LevelLoader
+		// {
+		// 	CenterGrid();
 
-			string [] lTestLevel = 
-			{
-				"#########",
-				"#/    # #",
-				"# . $   #",
-				"#   @ * #",
-				"#   $ . #",
-				"#  #   |#",
-				"#########"
-			};
+		// 	string [] lTestLevel = 
+		// 	{
+		// 		"#########",
+		// 		"#/    # #",
+		// 		"# . $   #",
+		// 		"#   @ * #",
+		// 		"#   $ . #",
+		// 		"#  #   |#",
+		// 		"#########"
+		// 	};
 
-			for(int y = 0; y < GRID_HEIGHT; y++)
-			{
-				for(int x = 0; x < GRID_WIDTH; x++)
-				{
-					char lTile = lTestLevel[y][x];
+		// 	for(int y = 0; y < GRID_HEIGHT; y++)
+		// 	{
+		// 		for(int x = 0; x < GRID_WIDTH; x++)
+		// 		{
+		// 			char lTile = lTestLevel[y][x];
 
-					Cell lCell = Utils.Spawner(cellScene, x, y, objectsContainer) as Cell;
+		// 			Cell lCell = Utils.Spawner(cellScene, x, y, objectsContainer) as Cell;
 
-					grid[x, y] = lCell;
+		// 			grid[x, y] = lCell;
 
-					GameObject lObj = null; 
+		// 			GameObject lObj = null; 
 
-					switch(lTile)
-					{
-						case '@':
-							lObj = Utils.Spawner(playerScene, x, y, objectsContainer) as Player;
-							player = lObj as Player;
-							break;
-						case '$':
-							lObj = Utils.Spawner(boxScene, x, y, objectsContainer) as BoxTesla;
-							break;
-						case '#':
-							lObj = Utils.Spawner(wallScene, x, y, objectsContainer) as Wall;
-							break;
-						case '*':
-							lObj = Utils.Spawner(electricWallScene, x, y, objectsContainer) as ElectricWall;
-							break;
-						case '.':
-							lObj = Utils.Spawner(goalBulbScene, x, y, objectsContainer) as GoalBulb;
-							break;
-						case '/':
-							lObj = Utils.Spawner(generatorScene, x, y, objectsContainer) as Generator;
-							break;
-						case '|':
-							lObj = Utils.Spawner(doorScene, x, y, objectsContainer) as Door;
-							break;
-					}
+		// 			switch(lTile)
+		// 			{
+		// 				case '@':
+		// 					lObj = Utils.Spawner(playerScene, x, y, objectsContainer) as Player;
+		// 					player = lObj as Player;
+		// 					break;
+		// 				case '$':
+		// 					lObj = Utils.Spawner(boxScene, x, y, objectsContainer) as BoxTesla;
+		// 					break;
+		// 				case '#':
+		// 					lObj = Utils.Spawner(wallScene, x, y, objectsContainer) as Wall;
+		// 					break;
+		// 				case '*':
+		// 					lObj = Utils.Spawner(electricWallScene, x, y, objectsContainer) as ElectricWall;
+		// 					break;
+		// 				case '.':
+		// 					lObj = Utils.Spawner(goalBulbScene, x, y, objectsContainer) as GoalBulb;
+		// 					break;
+		// 				case '/':
+		// 					lObj = Utils.Spawner(generatorScene, x, y, objectsContainer) as Generator;
+		// 					break;
+		// 				case '|':
+		// 					lObj = Utils.Spawner(doorScene, x, y, objectsContainer) as Door;
+		// 					break;
+		// 			}
 
-					if(lObj != null)
-					{
-						lCell.SetContent(lObj);
-						lObj.SetCell(lCell);
-						lObj.Init(x, y);
-					}
-				}
-			}
+		// 			if(lObj != null)
+		// 			{
+		// 				lCell.SetContent(lObj);
+		// 				lObj.SetCell(lCell);
+		// 				lObj.Init(x, y);
+		// 			}
+		// 		}
+		// 	}
 
-			PrintGrid();
+		// 	PrintGrid();
 				
-		}
+		// }
 
 		public void CenterGrid()
 		{
@@ -143,6 +141,7 @@ namespace Com.IsartDigital.SokoVolt.Managers {
 		public void OnMovePlayer(Vector2 pPlayerDirection)
 		{
 			MovePlayer((int)pPlayerDirection.X, (int)pPlayerDirection.Y);
+			GD.PrintErr("zgeg"); 
 		}
 
 		private void MovePlayer(int pDx, int pDy)
@@ -150,8 +149,8 @@ namespace Com.IsartDigital.SokoVolt.Managers {
             int lNewX = player.x + pDx;
 			int lNewY = player.y + pDy;
 
-			if (lNewX < 0 || lNewX >= GRID_WIDTH || lNewY < 0 || lNewY >= GRID_HEIGHT)
-				return;
+			// if (lNewX < 0 || lNewX >= GRID_WIDTH || lNewY < 0 || lNewY >= GRID_HEIGHT)
+			// 	return;
 
 			Cell lNewCell = grid[lNewX, lNewY];
 			GameObject lContent = lNewCell.GetContent();
