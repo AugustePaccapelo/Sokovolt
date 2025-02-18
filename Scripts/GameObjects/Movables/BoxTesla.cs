230ba396c0106ccb479772cd805b658a142f8037
Shiva <wizi97131@gmail.com>
2025-02-18 18:09:13 +0100

    tesla connections détection  des positions

--- Scripts/GameObjects/Movables/BoxTesla.cs
+++ Scripts/GameObjects/Movables/BoxTesla.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlTypes;
+using Com.IsartDigital.SokoVolt.Managers;
 
 // Author : Ferlat Thibaud 
 
@@ -8,10 +10,17 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
 	
 	public partial class BoxTesla : Movable
 	{
+		static List<BoxTesla> boxTeslasList = new List<BoxTesla>();
+        [Export] private int teslaRange;
+		[Export] public bool  energize { get; private set; }
+        GridManager gridManager = GridManager.GetInstance();
 		public override void _Ready()
-		{
+        {
 
-		}
+            ConnectionSearch();
+
+
+        }
 
 		public override void _Process(double pDelta)
 		{
@@ -27,6 +36,23 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
 
 			lSignals.EmitSignal(CustomSignals.SignalName.BoxTeslaMoved);
         }
+
+        private void ConnectionSearch()
+        {
+            Vector2 cellPosition = Utils.GetCellPos(this);
+
+
+            int x = (int)cellPosition.X;
+            int y = (int)cellPosition.Y;
+
+            
+			GD.Print(x+","+y);
+
+
+
+        }
+
+
 
         protected override void Dispose(bool pDisposing)
 		{
