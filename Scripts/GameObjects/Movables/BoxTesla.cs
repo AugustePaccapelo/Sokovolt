1d5eb68ddbfd25a7bdb37e416613d754c4578c56
William_SOUKAI <w.soukai@student.isartdigital.com>
2025-02-20 15:29:47 +0100

    détection  v1 up date

--- Scripts/GameObjects/Movables/BoxTesla.cs
+++ Scripts/GameObjects/Movables/BoxTesla.cs
@@ -16,6 +16,18 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
           
             [Export] public bool  energize { get; private set; }
             GridManager gridManager = GridManager.GetInstance();
+            private List<Vector2> directionScan = new List<Vector2>()
+            {  
+                Vector2.Up,
+                new Vector2(1,1),
+                new Vector2(-1,1),
+                new Vector2(1,-1),
+                new Vector2(-1,-1),
+                Vector2.Down,
+                Vector2.Left,
+                Vector2.Right
+            };
+            private int length ;
 
 			//Range tesla gestion 
 			public int range{get; private set;}
@@ -24,10 +36,11 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
             public override void _Ready()
             {
 				Init(); 
+               
             }
 
-			
-            public override void _Process(double pDelta)
+
+        public override void _Process(double pDelta)
             {
                 float lDelta = (float)pDelta;
 
@@ -38,8 +51,9 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
 				CallDeferred(nameof(UpdateRangeLabel));
 				
 				ConnectionSearch();
-				
-			}
+               length=directionScan.Count;
+
+            }
 
 			private void UpdateRangeLabel()
 			{
@@ -64,18 +78,56 @@ namespace Com.IsartDigital.SokoVolt.GameObjects.Movables {
 
             private void ConnectionSearch()
             {
+
+                Cell[,] lGrid = gridManager.grid;
                 Vector2 lCellPosition = Utils.GetCellPos(this);
+                int lLength = length;
+               List<Vector2> lcurentDirectionScan = new List<Vector2>(directionScan);
+
+               GD.Print(lGrid [1,1].GetContent().GetType());
 
 
                 int x = (int)lCellPosition.X;
                 int y = (int)lCellPosition.Y;
 
-            
-                // GD.Print(x+","+y);
 
+                for (int i =1 ; i <= range; i++)
+                {
+                    for (int j = lLength - 1; j >= 0; j--)
+                    {
+                        Vector2 scan = lcurentDirectionScan[j]*i;
+                        // = lGrid[(int)scan.X,(int)scan.Y];
+                        if ( this is BoxTesla lTesla)
+                        {
+                            if (lTesla.energize is true)
+                            {
+                                
+                                return;
 
-
+                            }
+                        }
+                        else if (this is Wall )
+                        {
+                            lcurentDirectionScan.RemoveAt(j);
+                        }
+                    }
+                }
             }
+            public static (int, int)? TrouverPosition<T>(T[,] tableau, T element)
+            {
+                for (int i = 0; i < tableau.GetLength(0); i++) 
+                {
+                    for (int j = 0; j < tableau.GetLength(1); j++) 
+                    {
+                        if (EqualityComparer<T>.Default.Equals(tableau[i, j], element))
+                        {
+                            return (i, j); 
+                        }
+                    }
+                }
+                return null;
+            }
+
 
 
 
