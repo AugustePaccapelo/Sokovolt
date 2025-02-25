d6b61312ab12f979794702d2814c306bf8ce6e70
Noé SALES <n.sales@student.isartdigital.com>
2025-02-25 11:22:27 +0100

    Tween correction

--- Scripts/GameObjects/Padlock.cs
+++ Scripts/GameObjects/Padlock.cs
@@ -13,22 +13,17 @@ namespace Com.IsartDigital.SokoVolt {
 		[Export] private LevelSelectorTesla tesla;
 		[Export] private PointLight2D light;
 		[Export] private PointLight2D topLight;
-		[Export] private PointLight2D electricLight1;
-		[Export] private PointLight2D electricLight2;
-		[Export] private PointLight2D electricLight3;
+		[Export] private Sprite2D[] listLock;
+		[Export] private PointLight2D[] listLight;
 
 		public void Open()
 		{
 			Tween lTween = CreateTween().SetParallel(true);
-			lTween.TweenProperty(topLock, "position", new Vector2(0, -100), 0.5f).AsRelative();
-			lTween.TweenProperty(topLock, "self_modulate", new Color(0,1,0), 0.5f);
-			lTween.TweenProperty(botLock, "self_modulate", new Color(0,1,0), 0.5f);
+			foreach (Sprite2D lockPart in listLock) lTween.TweenProperty(lockPart, "self_modulate", new Color(0, 1, 0), 0.5f);
+            lTween.TweenProperty(topLock, "position", new Vector2(0, -100), 0.5f).AsRelative();
 			lTween.TweenProperty(tesla, "modulate", new Color(0.5f,0.5f,0.5f), 0.5f);
-			lTween.TweenProperty(light, "energy", 3, 0.5f);
-			lTween.TweenProperty(topLight, "energy", 5, 2f);
-			lTween.TweenProperty(electricLight1, "energy", 3, 1.4f);
-			lTween.TweenProperty(electricLight2, "energy", 3, 0.7f);
-			lTween.TweenProperty(electricLight3, "energy", 3, 0.1f);
+			lTween.TweenProperty(topLight, "energy", 5, 1f);
+			foreach (PointLight2D light in listLight) lTween.TweenProperty(light, "energy", 3, 1);
 			lTween.SetParallel(false);
             lTween.TweenProperty(this, "modulate", new Color(0, 0, 0, -1), 0.5f).AsRelative();
         }
@@ -39,15 +34,10 @@ namespace Com.IsartDigital.SokoVolt {
             lTween.TweenProperty(this, "modulate", new Color(0, 0, 0, 1), 0.5f).AsRelative();
             lTween.SetParallel(true);
             lTween.TweenProperty(topLock, "position", new Vector2(0, 100), 0.5f).AsRelative();
-            lTween.TweenProperty(topLock, "self_modulate", new Color(0.62f, 0.10f, 0.09f), 0.5f);
-            lTween.TweenProperty(botLock, "self_modulate", new Color(0.62f, 0.10f, 0.09f), 0.5f);
+            foreach (Sprite2D lockPart in listLock) lTween.TweenProperty(lockPart, "self_modulate", new Color(0.62f, 0.10f, 0.09f), 0.5f);
             lTween.TweenProperty(tesla, "modulate", new Color(0.36f, 0.36f, 0.36f), 0.5f);
-            lTween.TweenProperty(light, "energy", 0, 0.5f);
+            foreach (PointLight2D light in listLight) lTween.TweenProperty(light, "energy", 0, 0.5f);
             lTween.TweenProperty(topLight, "energy", 0, 0.5f);
-            lTween.TweenProperty(electricLight1, "energy", 0, 0.5f);
-            lTween.TweenProperty(electricLight2, "energy", 0, 0.5f);
-            lTween.TweenProperty(electricLight3, "energy", 0, 0.5f);
-
         }
     }
 }
