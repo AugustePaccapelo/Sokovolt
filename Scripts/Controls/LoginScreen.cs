cf49465207d289b1e26cc40ceea54d4bb56e8c7a
Paccapelo Auguste <paccapelo.auguste@gmail.com>
2025-02-28 12:19:50 +0100

    Placeholder functions
    Login screen and Create account screen
    Functions ready to be filled for encryption and data save

--- Scripts/Controls/LoginScreen.cs
+++ Scripts/Controls/LoginScreen.cs
@@ -1,9 +1,11 @@
+using Com.IsartDigital.SokoVolt.Managers;
 using Godot;
 using System;
+using System.Collections.Generic;
 
 // Author : Auguste Paccapelo
 
-namespace Com.IsartDigital.
+namespace Com.IsartDigital.SokoVolt
 {
 	public partial class LoginScreen : Control
 	{
@@ -24,14 +26,32 @@ namespace Com.IsartDigital.
 		// ----- Paths ----- \\
 
 		// ----- Nodes ----- \\
+		[Export] private Control loginNode;
+		[Export] private TextEdit inputLoginUsername;
+        [Export] private TextEdit inputLoginPassword;
+        [Export] private Button buttonLoginConfirm;
+        [Export] private Button buttonLoginGoCreate;
 
-		// ----- Others ----- \\
+		[Export] private Control createNode;
+		[Export] private TextEdit inputCreateUsername;
+        [Export] private TextEdit inputCreatePassword;
+        [Export] private TextEdit inputCreateConfirmPassword;
+		[Export] private Button buttonCreateConfirm;
+		[Export] private Button buttonCreateGoLogin;
 
-		// ---------- FONCTIONS ---------- \\
+		private UIManager uiManager;
 
-		// ----- Constructor & Ready & Process ----- \\
+        // ----- Others ----- \\
+        [Signal] public delegate void StartGameEventHandler();
 
-		private LoginScreen() : base() { }
+        private const string USERNAME_KEY = "Username";
+		private const string PASSWORD_KEY = "Password";
+
+		private Dictionary<string, string> loginInfo = new Dictionary<string, string>();
+
+        // ---------- FUNCTIONS ---------- \\
+
+        // ----- Ready & Process ----- \\
 
 		public override void _Ready()
 		{
@@ -49,6 +69,20 @@ namespace Com.IsartDigital.
 			#endregion
 
 			base._Ready();
+
+			CustomMinimumSize = GetViewportRect().Size;
+
+			uiManager = GetParent<UIManager>();
+
+            StartGame += uiManager.GameStart;
+
+            loginInfo.Add(USERNAME_KEY, "");
+			loginInfo.Add(PASSWORD_KEY, "");
+
+			buttonLoginGoCreate.Pressed += ButtonChangeToCreate;
+			buttonCreateGoLogin.Pressed += ButtonChangeToLogin;
+            buttonLoginConfirm.Pressed += ButtonPressedLogin;
+			buttonCreateConfirm.Pressed += ButtonPressedCreate;
 		}
 
 		public override void _Process(double pDelta)
@@ -58,7 +92,45 @@ namespace Com.IsartDigital.
 			base._Process(lDelta);
 		}
 
-		// ----- My Fonctions ----- \\
+		// ----- My Functions ----- \\
+
+		private void ButtonPressedLogin()
+		{
+			loginInfo[USERNAME_KEY] = inputLoginUsername.Text;
+			loginInfo[PASSWORD_KEY] = inputLoginPassword.Text;
+			GD.Print("Trying to login with: ", loginInfo[USERNAME_KEY], " and ", loginInfo[PASSWORD_KEY]);
+			EmitSignal(SignalName.StartGame);
+			QueueFree();
+		}
+
+		private void ButtonPressedCreate()
+		{
+			string lPassword = inputCreatePassword.Text;
+			string lPasswordConfirm = inputCreateConfirmPassword.Text;
+			if (lPassword != lPasswordConfirm)
+			{
+				GD.Print("Not same password !");
+				return;
+			}
+
+			loginInfo[USERNAME_KEY] = inputCreateUsername.Text;
+            loginInfo[PASSWORD_KEY] = inputCreatePassword.Text;
+			GD.Print("creating account with: ", loginInfo[USERNAME_KEY], " and ", loginInfo[PASSWORD_KEY]);
+            EmitSignal(SignalName.StartGame);
+			QueueFree();
+        }
+
+		private void ButtonChangeToLogin()
+		{
+			createNode.Hide();
+			loginNode.Show();
+		}
+
+		private void ButtonChangeToCreate()
+		{
+			loginNode.Hide();
+			createNode.Show();
+		}
 
 		// ----- Destructor ----- \\
 
