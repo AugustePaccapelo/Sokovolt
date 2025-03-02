5c895d8c1624a5bdbb968ceee37492ea8a37fb7d
Ferlat Thibaud <t.ferlat@student.isartdigital.com>
2025-03-02 20:02:24 +0100

    Fully functional step counter display with undo redo

--- Scripts/Controls/HUD.cs
+++ Scripts/Controls/HUD.cs
@@ -1,8 +1,10 @@
+using Com.IsartDigital.SokoVolt.Managers;
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 //Author : Ferlat Thibaud 
-namespace Com.IsartDigital.ProjectName{
+namespace Com.IsartDigital.SokoVolt{
 	
 	public partial class HUD : Control
 	{
@@ -17,7 +19,11 @@ namespace Com.IsartDigital.ProjectName{
 		private HUD ():base() {}
 		#endregion
 
-		[Export] private Button undoButton, redoButton, mainMenuButton;
+		[Signal] public delegate void UndoButtonEventHandler();
+		[Signal] public delegate void RedoButtonEventHandler();
+		[Signal] public delegate void MainMenuButtonEventHandler();
+		[Export] public Button undoButton, redoButton, mainMenuButton;
+		[Export] public Label scoreLabel, stepLabel; 
 		public override void _Ready()
 		{
 			#region instance
@@ -34,6 +40,9 @@ namespace Com.IsartDigital.ProjectName{
 		private void Init()
 		{
 			CustomMinimumSize = GetViewportRect().Size;	
+			undoButton.Pressed += () => EmitSignal(nameof(UndoButton));
+			redoButton.Pressed += () => EmitSignal(nameof(RedoButton));
+			mainMenuButton.Pressed += () => EmitSignal(nameof(MainMenuButton));
 		}
 
 		public override void _Process(double pDelta)
--- Scripts/Managers/UIManager.cs
+++ Scripts/Managers/UIManager.cs
@@ -20,12 +20,10 @@ namespace Com.IsartDigital.SokoVolt.Managers
 		#endregion
 		[Export] PackedScene levelSelectorScene;
 		[Export] PackedScene mainMenuScene;
-		[Export] Button mainMenuButton;
 
 		private LevelSelector levelSelector;
 		private MainMenu mainMenu;
-
-		[Signal] public delegate void MainMenuButtonEventHandler();
+		private HUD hud; 
 
 		public override void _Ready()
 		{
@@ -43,10 +41,9 @@ namespace Com.IsartDigital.SokoVolt.Managers
 			base._Ready();
 
 			MainMenu.GetInstance();
+			hud = HUD.GetInstance();
 
-			mainMenuButton.Pressed += () => EmitSignal(nameof(MainMenuButton));
-
-			MainMenuButton += BackToMainMenu;
+			hud.MainMenuButton += BackToMainMenu;
 		}
 
 		public void GameStart() //Execute when StartButton is press in MainMenu
@@ -54,7 +51,7 @@ namespace Com.IsartDigital.SokoVolt.Managers
 			MainMenu.GetInstance().QueueFree();
 			levelSelector = levelSelectorScene.Instantiate() as LevelSelector;
 			AddChild(levelSelector);
-			mainMenuButton.Show();
+			hud.mainMenuButton.Show();
 		}
 
 		private void BackToMainMenu()
@@ -62,7 +59,7 @@ namespace Com.IsartDigital.SokoVolt.Managers
 			LevelSelector.GetInstance().QueueFree();
 			mainMenu = mainMenuScene.Instantiate() as MainMenu;
 			AddChild(mainMenu);
-			mainMenuButton.Hide();
+			hud.mainMenuButton.Hide();
 		}
 
 		protected override void Dispose(bool pDisposing)
