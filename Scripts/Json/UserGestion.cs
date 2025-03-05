5dd15ae6f1f5340f69ee8b79798d6a0a6358abee
Angelo <angelodylan16@gmail.com>
2025-03-05 20:34:00 +0100

    added json file

--- Scripts/Json/UserGestion.cs
+++ Scripts/Json/UserGestion.cs
@@ -29,8 +29,8 @@ namespace Com.IsartDigital.ProjectName
 		}
 		#endregion
 
-		private string jsonFilePath = ProjectSettings.GlobalizePath("user://"); // GlobalizePath gives the correct path for each operating system, "user://" is the static path for where the game/games data is saved
-		private string json;
+		private string jsonFilePath = ProjectSettings.GlobalizePath("user://Json//UserData.Json");
+        private Godot.Collections.Dictionary<string, Variant> usersData = new Godot.Collections.Dictionary<string, Variant>();
 
 		public override void _Ready()
 		{
@@ -46,65 +46,92 @@ namespace Com.IsartDigital.ProjectName
 			#endregion
 
 			LoginScreen.GetInstance().userGestion = this;
-		}
+			LoadUsers();
 
-		private class User
+			string testName = "Test";
+            string testPassword = "Test123";
+
+			bool registered = RegisterUser(testName, testPassword);
+			GD.Print(registered ? "User 'Test' added." : "User 'Test' already exist!");
+
+			bool loginSuccess = LoginUser(testName, testPassword);
+			GD.Print(loginSuccess ? "Login successful for 'Test'" : "Login failed for 'Test'");
+        }
+
+        private class User
 		{
 			public string name { get; set; }
 			public string password { get; set; }
 		}
 
-		private static string PasswordHashing(string pPassword) // to encrypt the password ==== pour Auguste
+        private static string PasswordHashing(string pPassword) // to encrypt the password par Auguste
+        {
+            SHA256 lSha256 = SHA256.Create();
+
+            byte[] lBytes = lSha256.ComputeHash(Encoding.UTF8.GetBytes(pPassword));
+
+            string lHashedPassword = "";
+            foreach (byte lByte in lBytes)
+            {
+                lHashedPassword += (lByte.ToString("x2"));
+            }
+
+            return lHashedPassword;
+        }
+
+        private void SaveUsers() // this saves users in a list in a json file
 		{
-			using (SHA256 pSha256 = SHA256.Create()) ; // to use
-			return pPassword;
+			string lJson = Json.Stringify(usersData);
+			JsonTool.WriteToFile(jsonFilePath, lJson); // use JsonTool instead of File.WriteAllText
 		}
 
-		//private void SaveTextToFile() // to finish
+		//private List<User> GetUsers() // this fetches registered users by reading the json file
 		//{
+		//	if (!File.Exists(jsonFilePath))
+		//	{
+		//		GD.Print("Creating new users file");
+		//		//JsonTool.WriteToFile(jsonFilePath); // ??
+		//	}
 
+  //          string lJson = JsonTool.ReadFileContents(jsonFilePath);
+		//	//json = File.ReadAllText(jsonFilePath);
+		//	if (string.IsNullOrEmpty(jsonFilePath)) return new List<User>();
+  //          return JsonSerializer.Deserialize<List<User>>(lJson) ?? new List<User>();
 		//}
 
-		private void SaveUsers(List<User> pUsers) // this saves users in a list in a json file
-		{
-			json = JsonSerializer.Serialize(pUsers, new JsonSerializerOptions { WriteIndented = true });
-			JsonTool.WriteToFile(jsonFilePath, json); // use JsonTool instead of File.WriteAllText
-		}
-
-		private List<User> GetUsers() // this fetches registered users by reading the json file
-		{
+		private void LoadUsers() // this fetches registered users by reading the json file
+        {
 			if (!File.Exists(jsonFilePath))
 			{
-				GD.Print("Creating new users file");
-				//JsonTool.WriteToFile(jsonFilePath); // ??
+				GD.Print("Creating new user file");
+				SaveUsers();
 			}
-
-            json = JsonTool.ReadFileContents(jsonFilePath);
-			//json = File.ReadAllText(jsonFilePath);
-			if (string.IsNullOrEmpty(jsonFilePath)) return new List<User>();
-            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+			else
+			{
+				string lJson = JsonTool.ReadFileContents(jsonFilePath);
+				var lParsed = Json.ParseString(lJson);
+				if (lParsed.VariantType == Variant.Type.Dictionary) usersData = lParsed.As<Godot.Collections.Dictionary<string, Variant>>();
+			}
 		}
 
 		public bool RegisterUser(string pName, string pPassword) // register new users 
 		{
-			List<User> pUsers = GetUsers();
-			if (pUsers.Exists(pU => pU.name == pName))
+			if (usersData.ContainsKey(pName))
 			{
 				GD.Print("Username already taken!");
 				return false;
 			}
 
-			pUsers.Add(new User { name = pName, password = PasswordHashing(pPassword) });
-			SaveUsers(pUsers);
+			usersData[pName] = PasswordHashing(pPassword);
+			SaveUsers();
 			GD.Print("User registered");
-			return true;
+			return true;	
 		}
 
 		public bool LoginUser(string pName, string pPassword) // connects the users 
 		{
-			List<User> pUsers = GetUsers();
 			string pHashedPwd = PasswordHashing(pPassword);
-			if (pUsers.Exists(pU => pU.name == pName && pU.password == pPassword))
+			if (usersData.TryGetValue(pName, out Variant pSavedPwd) && pSavedPwd.AsString() == pHashedPwd)
 			{
 				GD.Print("Login successful");
 				return true;
