5e00032a1bcd35a452b11459c9ee71c588d03797
Noé SALES <n.sales@student.isartdigital.com>
2025-03-10 12:00:15 +0100

    size changed

--- Scripts/Tools/Utils.cs
+++ Scripts/Tools/Utils.cs
@@ -8,8 +8,8 @@ namespace Com.IsartDigital.SokoVolt {
 	
 	public static class Utils 
 	{
-		public const int TILE_WIDTH = 94;
-		public const int TILE_HEIGHT = 54;
+		public const int TILE_WIDTH = 153;
+		public const int TILE_HEIGHT = 90;
 
 		public static Node2D Spawner(PackedScene pScene, int pX, int pY, Node2D pParent)
 		{
