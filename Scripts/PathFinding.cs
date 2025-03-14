4f0f130bb1616db2caa3a93cb7a33640dabbbf34
Angelo <angelodylan16@gmail.com>
2025-03-14 14:24:38 +0100

    Added script PathFinding

---
+++ Scripts/PathFinding.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+// Author : A. Dylan Montenegro Utrela
+
+namespace Com.IsartDigital.ProjectName {
+	
+	public partial class PathFinding : Node
+	{
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
