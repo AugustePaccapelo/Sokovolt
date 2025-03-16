d8f976c8b36c32766962d0f421d8150117c4f145
Sales Noe <n.sales@student.isartdigital.com>
2025-03-16 17:03:34 +0100

    Redo LevelSelectorTesla sprite and change juciness

--- Scripts/GameObjects/LevelSelectorBolt.cs
+++ Scripts/GameObjects/LevelSelectorBolt.cs
@@ -8,8 +8,8 @@ namespace Com.IsartDigital.SokoVolt.GameObjects {
 	public partial class LevelSelectorBolt : Node2D
 	{
 		[Export] public AnimationPlayer animationPlayer;
-        [Export] public GpuParticles2D sparks;
-        [Export] public GpuParticles2D flare;
+        //[Export] public GpuParticles2D sparks;
+        //[Export] public GpuParticles2D flare;
         [Export] public Line2D bolt;
         [Export] public PointLight2D light;
         [Export] public bool animationActive = true;
@@ -17,8 +17,8 @@ namespace Com.IsartDigital.SokoVolt.GameObjects {
         public override void _Ready()
         {
             //animationPlayer.Active = animationActive;
-            sparks.Emitting = false;
-            flare.Emitting = false;
+            //sparks.Emitting = false;
+            //flare.Emitting = false;
             bolt.Hide();
             light.Energy = 0;
         }
